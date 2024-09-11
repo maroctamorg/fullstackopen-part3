367c56430c2794e5f09d9b5d73194160b6d3887e
maroctamorg <guimaraesneto.marcelo@gmail.com>
2024-09-11 11:53:37 +0300

    exercise 3

--- index.js
+++ index.js
@@ -27,6 +27,15 @@ app.get('/api/persons', (req, res) => {
     res.json(persons)
 })
 
+app.get('/api/persons/:id', (req, res) => {
+    const person = persons.find(person => person.id === req.params.id)
+    if(!person) {
+        return res.status(404).end()
+    }
+
+    res.json(person)
+})
+
 app.get('/info', (req, res) => {
     res.send(`
         <p>Phonebook has info for ${persons.length} people</p>
