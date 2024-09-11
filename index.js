9271be6a0ef0669f91023937631ccdc5654d751d
maroctamorg <guimaraesneto.marcelo@gmail.com>
2024-09-11 14:35:04 +0300

    exercise 9

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const morgan = require('morgan');
 
 morgan.token('body', (req, res) => {
@@ -6,6 +7,7 @@ morgan.token('body', (req, res) => {
 })
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
 
