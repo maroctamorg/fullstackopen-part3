a9caffa2c16b9f9f5ba6f580fc6fa677d4f05d94
maroctamorg <guimaraesneto.marcelo@gmail.com>
2024-09-11 14:54:32 +0300

    exercise 10: fly.io deployment

---
+++ README.md
@@ -0,0 +1,2 @@
+# FULL STACK OPEN - PART 3
+fly.io deployment: [fullstackopen-part3.dev](https://fullstackopen-part3-icy-rain-2124.fly.dev/)
--- index.js
+++ index.js
@@ -90,6 +90,7 @@ app.post('/api/persons', (req, res) => {
     res.json(person)
 })
 
-app.listen(3001, () => {
-    console.log('Server running on port 3001')
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
 })
